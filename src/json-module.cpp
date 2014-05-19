8439e2b9dbdfd9a7073885ee3a7e775054ab5c74
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2014-05-19 10:59:07 +0000

    updated module sources to reflect MIT licenses; added license string to binary module's exported symbols for use with Qore >= 0.8.10
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/module-json/trunk@6490 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- src/json-module.cpp
+++ src/json-module.cpp
@@ -41,7 +41,12 @@ DLLEXPORT int qore_module_api_minor = QORE_MODULE_API_MINOR;
 DLLEXPORT qore_module_init_t qore_module_init = json_module_init;
 DLLEXPORT qore_module_ns_init_t qore_module_ns_init = json_module_ns_init;
 DLLEXPORT qore_module_delete_t qore_module_delete = json_module_delete;
+#ifdef _QORE_HAS_QL_MIT
+DLLEXPORT qore_license_t qore_module_license = QL_MIT;
+#else
 DLLEXPORT qore_license_t qore_module_license = QL_LGPL;
+#endif
+DLLEXPORT char qore_module_license_str[] = "MIT";
 
 QoreNamespace JNS("Json");
 
